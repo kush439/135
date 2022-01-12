7f073f9cf6bd4bcfcfca9a0590c1379fd30d2069
Kush D. Patel <80115450+kush439@users.noreply.github.com>
2022-01-12 10:23:50 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+
+function preload(){
+    loadVideo = "video.mp4";
+}
+
+function setup(){
+    canvas = createCanvas(380, 380);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(380, 380);
+    video.hide();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting objectes";
+}
